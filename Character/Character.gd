3ff29965691b803aa8a27648ddef941407df11ed
Tylere Couture <tylerecouture@gmail.com>
2023-07-03 16:12:59 -0700

    Bite marks when character is touched

--- Character/Character.gd
+++ Character/Character.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 export var DeathEffect : PackedScene
 export var BiteEffect : PackedScene
+export var BiteMarkEffect : PackedScene
 
 export var speed := 200
 export var Ammo : PackedScene
@@ -45,13 +46,20 @@ func _on_Detector_area_shape_entered(area_rid, area, area_shape_index, local_sha
 	get_tree().change_scene_to_file("res://sticks_and_stones.tscn")
 
 func _on_Detector_body_entered(body):
+	
 	print("OUCH!")
 	
 	var effect : Node2D = BiteEffect.instance()
 	get_tree().get_root().add_child(effect)
 	effect.position = body.global_position
+	
+	var bite_mark : Sprite = BiteMarkEffect.instance()
+	add_child(bite_mark)
+	var bite_position = body.global_position - (body.global_position - global_position)*0.7
+	bite_mark.global_position = bite_position
+	
 	body.queue_free()
-
+	
 	health -= 1
 	$HealthHUD.update_healthbar(health)
 	if health <= 0:
