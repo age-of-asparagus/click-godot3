07352f19b69f6211947f41130f45e71bf1e4434f
Tylere Couture <tylerecouture@gmail.com>
2023-07-02 18:54:48 -0700

    Huge splatter when player dies

--- Effects/BloodSplatter.gd
+++ Effects/BloodSplatter.gd
@@ -1,22 +1,21 @@
-extends Node2D
+extends CPUParticles2D
+
 var is_fading := false
 var fade_alpha := 1.0
-export var fade_speed := 1
+export var fade_speed := 1.0
 
 func _ready():
-	$CPUParticles2D.emitting = true
+	emitting = true
 	
 func _process(delta):
 	if is_fading:
 		fade_alpha -= fade_speed * delta
 		fade_alpha = max(fade_alpha, 0)
-		print(fade_alpha)
 
 		# Set the particles' color and alpha
-		var particles_color : Color = $CPUParticles2D.color
+		var particles_color : Color = color
 		particles_color.a = fade_alpha
-		$CPUParticles2D.color = particles_color
-		print($CPUParticles2D.color)
+		color = particles_color
 
 		# Check if the fade-out animation is completed
 		if fade_alpha <= 0:
