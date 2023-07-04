ddd93f285af8095ac2bedd456224bbe0919483f9
Tylere Couture <tylerecouture@gmail.com>
2023-07-04 14:24:02 -0700

    F key to interact

--- Overworld/Overworld.gd
+++ Overworld/Overworld.gd
@@ -1,8 +1,18 @@
 extends Node2D
 
+var player_is_near := false
 
+func _process(delta):
+	if player_is_near:
+		$ClickArea/KeyPrompt.visible = true
+		if Input.is_action_just_pressed("interact"):
+			get_tree().change_scene("res://minigames/sticks_and_Stones.tscn")
+	else:
+		$ClickArea/KeyPrompt.visible = false
+	
 
 func _on_ClickArea_body_entered(body):
-	# change scenes
+	player_is_near = true
 	
-	get_tree().change_scene("res://minigames/sticks_and_Stones.tscn")
+func _on_ClickArea_body_exited(body):
+	player_is_near = false
